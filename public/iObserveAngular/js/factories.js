6c767de54c31b376d550a6d9b74d60d31c02e0d1
Richard Nesnass <richardn@uio.im>
2013-07-31 18:14:29 +0200

    Extract factories to a dedicated file. Include directive to place appropriate chart from charts.js when called. Improve navigation tabs and UI operation.

---
+++ public/iObserveAngular/js/factories.js
@@ -0,0 +1,338 @@
+//var iObserveApp = angular.module('iObserveApp', ['ngResource', 'ngSanitize', 'ui.bootstrap', 'ngUpload', 'ngDragDrop']);
+var iObserveApp = angular.module('iObserveApp', ['ngResource', 'ngSanitize', 'ui.bootstrap', 'ngUpload'], null);
+
+// var routePrePath = "http://observe.uio.im";
+var routePrePath = "";
+
+var postConfiguration = {
+    type: "POST",
+    contentType: 'application/json',
+    dataType: "json",
+    async: true,
+    processData: false
+}
+var putConfiguration = {
+    type: "PUT",
+    contentType: 'application/json',
+    dataType: "json",
+    async: true,
+    processData: false
+}
+var getConfiguration = {
+    type: "GET",
+    contentType: 'application/json',
+    dataType: "json",
+    async: true,
+    processData: false
+}
+var deleteConfiguration = {
+    type: "DELETE",
+    contentType: 'application/json',
+    dataType: "json",
+    async: true,
+    processData: false
+}
+
+iObserveApp.factory('iObserveData', function ($http, $q) {
+
+    var currentUserId = null;
+    var spaceListObject = null;
+    var selectedSpace = null;
+    var sessionListObject = null;
+    var selectedSession = null;
+    var chartDataObject = null;
+    var studyIdToDelete = null;
+
+    var requestSessionListObject = function() {
+        var deferred = $q.defer();
+        var route = routePrePath + "/space/" + spaceListObject.space_id + "/session";
+
+        $http.get(route, getConfiguration).success(function(data) {
+            deferred.resolve(data);
+        }).error(function(data, status){
+                alert( "Request failed: " + data.message  );
+                deferred.reject();
+            });
+        return deferred.promise;
+    };
+
+    var requestEventListObject = function() {
+        var deferred = $q.defer();
+
+        $http.get(routePrePath + "/session/" + currentUserId + "/space", getConfiguration).success(function(data) {
+            deferred.resolve(data);
+        }).error(function(data, status){
+                alert( "Request failed: " + data.message  );
+                deferred.reject();
+            });
+        return deferred.promise;
+    };
+
+    var requestStudyListObject = function() {
+        var deferred = $q.defer();
+
+        $http.get(routePrePath + "/user/" + currentUserId + "/space", getConfiguration).success(function(data) {
+            deferred.resolve(data);
+        }).error(function(data, status){
+                alert( "Request failed: " + data.message  );
+                deferred.reject();
+            });
+        return deferred.promise;
+    };
+
+    var requestNewStudyObject = function(data) {
+        var deferred = $q.defer();
+
+        $http.post(routePrePath + "/user/" + currentUserId + "/space", data, getConfiguration).success(function(data) {
+            deferred.resolve(data);
+        }).error(function(data, status){
+                alert( "Request failed: " + data.message  );
+                deferred.reject();
+            });
+        return deferred.promise;
+    };
+
+    var requestDeleteStudyObject = function(data) {
+        var deferred = $q.defer();
+
+        $http.delete(routePrePath + "/space/"+data, deleteConfiguration).success(function(data) {
+            deferred.resolve(data);
+        }).error(function(data, status){
+                alert( "Request failed: " + data.message  );
+                deferred.reject();
+            });
+        return deferred.promise;
+    };
+
+    var requestDeleteRoomObject = function(data) {
+        var deferred = $q.defer();
+
+        $http.delete(routePrePath + "/room/"+data, deleteConfiguration).success(function(data) {
+            deferred.resolve(data);
+        }).error(function(data, status){
+                alert( "Request failed: " + data.message  );
+                deferred.reject();
+            });
+        return deferred.promise;
+    };
+
+    var requestAddStudyRoomObject = function(data) {
+        var deferred = $q.defer();
+
+        $http.post(routePrePath + "/space/"+data.spaceid+"/room", data, postConfiguration).success(function(data) {
+            deferred.resolve(data);
+        }).error(function(data, status){
+                alert( "Request failed: " + data.message  );
+                deferred.reject();
+            });
+        return deferred.promise;
+    };
+
+    return {
+
+        setUserId: function(id) {
+            currentUserId = id;
+        },
+
+        sessionListObject : sessionListObject,
+
+        setSessionListObject: function() {
+            if(spaceListObject != null) {
+                requestSessionListObject().then(function(resultData) {
+                    sessionListObject = resultData;
+                    selectedSession = sessionListObject[0];
+                });
+            }
+            else
+                alert( "A space must be selected before sessions can be shown");
+        },
+
+        selectedSession: selectedSession,
+
+        spaceListObject: spaceListObject,
+
+        /*doGetSpaces: function(userId) {
+         requestSpaceListObject(userId).then(function(resultData) {
+         spaceListObject = resultData;
+         selectedSpace = spaceListObject[0];
+         });
+         },   */
+
+        doGetEvents: requestEventListObject,
+        doGetStudies: requestStudyListObject,
+        doNewStudy: requestNewStudyObject,
+        doDeleteStudy: requestDeleteStudyObject,
+        doCreateStudyRoom: requestAddStudyRoomObject,
+        doDeleteRoom: requestDeleteRoomObject,
+        selectedSpace: selectedSpace
+    }
+});
+
+iObserveApp.factory('iObserveStates', function ($http, $q) {
+
+    var navigationState= "iObserve";
+    var loginState = false;
+    var loginToken = "";
+    var userId = "";
+
+    var userLogout = function() {
+        navigationState= "iObserve";
+        userId = "";
+        loginToken = "";
+        loginState = false;
+    }
+
+    var userLogin = function(data) {
+        var deferred = $q.defer();
+
+        $http.post(routePrePath + '/login', data, postConfiguration).success(function(data) {
+            loginState = true;
+            loginToken = data.token;
+            userId = data.userId;
+            navigationState = "iObserve";
+            deferred.resolve(data);
+        }).error(function(data, status){
+                alert( "Request failed: " + data.message  );
+                deferred.reject();
+            });
+
+        return deferred.promise;
+    }
+
+    var userRegistration = function(data) {
+        var deferred = $q.defer();
+
+        $http.post(routePrePath + '/user', data, postConfiguration).success(function(data) {
+            userLogin(data);
+            deferred.resolve(data);
+        }).error(function(data, status){
+                alert( "Request failed: " + data.message );
+                deferred.reject();
+            });
+
+        return deferred.promise;
+    }
+
+    var getProfile = function() {
+
+        if(loginState) {
+            var deferred = $q.defer();
+
+            $http.get(routePrePath + '/user/'+userId, getConfiguration).success(function(data) {
+                deferred.resolve(data);
+            }).error(function(data, status){
+                    alert( "Request failed: " + data.message  );
+                    deferred.reject();
+                });
+
+            return deferred.promise;
+        }
+        else
+            return null;
+    }
+
+    var updateProfile = function(data) {
+        var deferred = $q.defer();
+
+        $http.put(routePrePath + '/user', data, postConfiguration).success(function(data) {
+            userLogin(data);
+            deferred.resolve(data);
+        }).error(function(data, status){
+                alert( "Request failed: " + data.message );
+                deferred.reject();
+            });
+
+        return deferred.promise;
+    }
+
+    return {
+        getNavigationState: function() {
+            return navigationState;
+        },
+        setNavigationState: function(divKey) {
+            navigationState = divKey;
+        },
+        getLoginState: function() {
+            return loginState;
+        },
+        setLoginState: function(state) {
+            loginState = state;
+        },
+        getSessionObject: function() {
+            return sessionObject;
+        },
+        setSessionObject: function(so) {
+            sessionObject = so;
+        },
+        getLoginToken: function() {
+            return loginToken;
+        },
+        setLoginToken: function(token) {
+            loginToken = token;
+        },
+        getUserId: function() {
+            return userId;
+        },
+        setUserId: function(id) {
+            userId = id;
+        },
+        doUpdateProfile : updateProfile,
+        doGetProfile : getProfile,
+        doUserRegistration: userRegistration,
+        doUserLogin: userLogin,
+        doUserLogout: userLogout
+    };
+});
+
+iObserveApp.factory('iObserveUtilities', function () {
+    var timeConverter = function($ts){
+        var a = new Date($ts*1000);
+        var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+        var year = a.getFullYear();
+        var month = months[a.getMonth()];
+        var date = a.getDate();
+        var hour = a.getHours();
+        var min = a.getMinutes();
+        var sec = a.getSeconds();
+        var time = date+','+month+' '+year+' '+hour+':'+min+':'+sec ;
+        return time;
+    };
+
+    var tDiff = function($a,$b) {
+        var a = new Date($a*1000);
+        var b = new Date($b*1000);
+
+        var nTotalDiff = b.getTime() - a.getTime();
+        var oDiff = new Object();
+
+        oDiff.days = Math.floor(nTotalDiff/1000/60/60/24);
+        nTotalDiff -= oDiff.days*1000*60*60*24;
+
+        oDiff.hours = Math.floor(nTotalDiff/1000/60/60);
+        nTotalDiff -= oDiff.hours*1000*60*60;
+
+        oDiff.minutes = Math.floor(nTotalDiff/1000/60);
+        nTotalDiff -= oDiff.minutes*1000*60;
+
+        oDiff.seconds = Math.floor(nTotalDiff/1000);
+
+        return oDiff;
+    };
+
+    return {
+        timeConverter : timeConverter,
+        tDiff : tDiff
+    }
+});
+
+//  Place a tag called <markdown></markdown> in a html file and this directive will convert any contained plain text to markup.
+iObserveApp.directive('markdown', function () {
+    var converter = new Showdown.converter();
+    return {
+        restrict: 'E',
+        link:function(scope,element,attrs) {
+            var htmlText = converter.makeHtml(element.text());
+            element.html(htmlText)
+        }
+    }
+})
--- public/iObserveAngular/js/statistics.js
+++ public/iObserveAngular/js/statistics.js
@@ -3,6 +3,7 @@ iObserveApp.controller('StatisticsCtrl', function($scope, iObserveStates, iObser
     $scope.sessionChartRequested = false;
     $scope.sessionListRequested = false;
     $scope.sessionChartRequested = false;
+    $scope.showChart = false;
 
     $scope.sessionChartList = iObserveCharting.sessionChartList;
     $scope.selectedChart = null;
@@ -17,16 +18,10 @@ iObserveApp.controller('StatisticsCtrl', function($scope, iObserveStates, iObser
         if($scope.currentStudy == $study || $scope.sessionChartRequested == false)
             $scope.studyChartRequested = !$scope.studyChartRequested;
         $scope.currentStudy = $study;
-
-
-
     };
 
     $scope.foldSessionChart = function($session) {
-
         $scope.sessionChartRequested = !$scope.sessionChartRequested;
-
-
     };
 
     $scope.foldSessions = function($study) {
@@ -35,25 +30,47 @@ iObserveApp.controller('StatisticsCtrl', function($scope, iObserveStates, iObser
         $scope.currentStudy = $study;
 
         $scope.sessions = $scope.currentStudy.sessionobs;
-
     };
 
     $scope.displayChart = function($chart) {
+        if($scope.selectedChart == $chart || $scope.showChart == false)
+            $scope.showChart = !$scope.showChart;
         $scope.selectedChart = $chart;
+        $scope.chartName = $chart.name;
+  //      var chartSpace = d3.select("#chartSpace");
+  //      iObserveCharting.selectedChart = $chart;
+
     };
 
-    $scope.displayChart = function($chart) {
-        $scope.sessionChartRequested = true;
-
-    };
+//    $scope.displayChart = function($chart) {
+//        $scope.sessionChartRequested = true;
+//    };
 })
 
-iObserveApp.directive('chartElement', function () {
+
+iObserveApp.directive('ioVisualization', function (iObserveCharting) {
     return {
         restrict: 'E',
-        link:function(scope,element,attrs) {
-            var htmlText = '<div>Another test</div>';
-            element.html(htmlText)
+        scope: {
+            val: '=',
+            grouped: '='
+        },
+        link: function (scope, element, attrs) {
+            // set up initial svg object
+            var vis = d3.select(element[0])
+                .append("svg")
+                .attr("width", 100)
+                .attr("height", 100);
+
+            scope.$watch('val', function (newVal, oldVal) {
+                if (!newVal || newVal === oldVal) {
+                    return;
+                }
+                vis.selectAll('*').remove();
+
+                iObserveCharting.plotChart(newVal, vis);
+
+            });
         }
     }
-})
+});
