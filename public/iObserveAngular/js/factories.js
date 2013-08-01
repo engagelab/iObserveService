31d7dc4e67f8a9626459be1f0e5b2a075f293047
Richard Nesnass <richardn@uio.im>
2013-08-01 11:32:46 +0200

    Fix VCS sync errors

--- public/iObserveAngular/js/factories.js
+++ public/iObserveAngular/js/factories.js
@@ -33,6 +33,47 @@ var deleteConfiguration = {
     processData: false
 }
 
+iObserveApp.factory('iObserveUtilities', function () {
+    var timeConverter = function($ts){
+        var a = new Date($ts*1000);
+        var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+        var year = a.getFullYear();
+        var month = months[a.getMonth()];
+        var date = a.getDate();
+        var hour = a.getHours();
+        var min = a.getMinutes();
+        var sec = a.getSeconds();
+        var time = date+','+month+' '+year+' '+hour+':'+min+':'+sec ;
+        return time;
+    };
+
+    var tDiff = function($a,$b) {
+        var a = new Date($a*1000);
+        var b = new Date($b*1000);
+
+        var nTotalDiff = b.getTime() - a.getTime();
+        var oDiff = new Object();
+
+        oDiff.days = Math.floor(nTotalDiff/1000/60/60/24);
+        nTotalDiff -= oDiff.days*1000*60*60*24;
+
+        oDiff.hours = Math.floor(nTotalDiff/1000/60/60);
+        nTotalDiff -= oDiff.hours*1000*60*60;
+
+        oDiff.minutes = Math.floor(nTotalDiff/1000/60);
+        nTotalDiff -= oDiff.minutes*1000*60;
+
+        oDiff.seconds = Math.floor(nTotalDiff/1000);
+
+        return oDiff;
+    };
+
+    return {
+        timeConverter : timeConverter,
+        tDiff : tDiff
+    }
+});
+
 iObserveApp.factory('iObserveData', function ($http, $q) {
 
     var currentUserId = null;
@@ -284,46 +325,8 @@ iObserveApp.factory('iObserveStates', function ($http, $q) {
     };
 });
 
-iObserveApp.factory('iObserveUtilities', function () {
-    var timeConverter = function($ts){
-        var a = new Date($ts*1000);
-        var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-        var year = a.getFullYear();
-        var month = months[a.getMonth()];
-        var date = a.getDate();
-        var hour = a.getHours();
-        var min = a.getMinutes();
-        var sec = a.getSeconds();
-        var time = date+','+month+' '+year+' '+hour+':'+min+':'+sec ;
-        return time;
-    };
 
-    var tDiff = function($a,$b) {
-        var a = new Date($a*1000);
-        var b = new Date($b*1000);
 
-        var nTotalDiff = b.getTime() - a.getTime();
-        var oDiff = new Object();
-
-        oDiff.days = Math.floor(nTotalDiff/1000/60/60/24);
-        nTotalDiff -= oDiff.days*1000*60*60*24;
-
-        oDiff.hours = Math.floor(nTotalDiff/1000/60/60);
-        nTotalDiff -= oDiff.hours*1000*60*60;
-
-        oDiff.minutes = Math.floor(nTotalDiff/1000/60);
-        nTotalDiff -= oDiff.minutes*1000*60;
-
-        oDiff.seconds = Math.floor(nTotalDiff/1000);
-
-        return oDiff;
-    };
-
-    return {
-        timeConverter : timeConverter,
-        tDiff : tDiff
-    }
-});
 
 //  Place a tag called <markdown></markdown> in a html file and this directive will convert any contained plain text to markup.
 iObserveApp.directive('markdown', function () {
