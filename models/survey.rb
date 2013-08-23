d8ed36bfd5fa41420a05983b9b18bd6fedac7e91
Jeremy Toussaint <jeremyt@uio.im>
2013-08-23 13:28:35 +0200

    survey service update

--- models/survey.rb
+++ models/survey.rb
@@ -7,6 +7,7 @@ class Survey
 
   has_many :questions
   field :questions, :type => Array, :default => []
+  field :answers, :type => Array, :default => []
   field :label, :type => String
   field :created_on, :type => Bignum
 end
